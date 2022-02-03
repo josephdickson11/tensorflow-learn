4a9d1b3d10f0792d7d37bfc2fe3a164ed909e6be
josephdickson11 <josepholadipupoibeun@gmail.com>
2022-02-03 10:15:50 +0100

    update testing file

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 
+from pyexpat import model
 import tensorflow as tf
 import numpy as np
 
 print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 
-xs = np.array([-1.0, 0.0, 2.0, 3.0, 4.0], dtype=float)
